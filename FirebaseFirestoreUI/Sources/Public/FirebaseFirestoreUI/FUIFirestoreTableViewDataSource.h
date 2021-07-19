ff068e7a54b0f0608173cf5a3847e0f1ed359264
Roscoe <roscoe@simplecohesion.com>
2021-07-20 09:35:25 +1000

    expose didUpdateWithDiff and populateCell

--- FirebaseFirestoreUI/Sources/Public/FirebaseFirestoreUI/FUIFirestoreTableViewDataSource.h
+++ FirebaseFirestoreUI/Sources/Public/FirebaseFirestoreUI/FUIFirestoreTableViewDataSource.h
@@ -32,6 +32,12 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface FUIFirestoreTableViewDataSource : NSObject <UITableViewDataSource>
 
+- (void)batchedArray:(FUIBatchedArray *)array
+   didUpdateWithDiff:(FUISnapshotArrayDiff<FIRDocumentSnapshot *> *)diff;
+
+@property (strong, nonatomic, readwrite) UITableViewCell *(^populateCell)
+(UITableView *tableView, NSIndexPath *indexPath, FIRDocumentSnapshot *snap);
+
 /**
  * The UITableView instance that operations (inserts, removals, moves, etc.) are
  * performed against. This collection view must be receiving data from
